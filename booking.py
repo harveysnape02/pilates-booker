b595346e8be47cd69ff8cf4a7a997c0ed4c1282c
Harvey Snape <harveysnape75@gmail.com>
2026-05-21 16:17:36 +0100

    Changed logs and added notifications

--- booking.py
+++ booking.py
@@ -4,14 +4,14 @@ from datetime import datetime, date, timedelta
 import re
 import zoneinfo
 import os
+import notification
 
-# 1. Get the current time explicitly in the UK timezone
+#Creates timezone aware date.
 uk_timezone = zoneinfo.ZoneInfo("Europe/London")
 now_in_uk = datetime.now(uk_timezone)
-# 2. Extract just the local calendar date
 today = now_in_uk.date()
-# 3. Calculate your target date (7 days from your true local day)
 targetDate = today + timedelta(days=7)
+
 defaultTimeout = 6000
 username = os.environ.get('MY_USERNAME')
 password = os.environ.get('MY_PASSWORD')
@@ -19,6 +19,11 @@ if not username or not password:
     print("Error: Missing username or password environment variables.")
     exit(1)
 bookingLink = f"https://bookings.better.org.uk/location/better-gym-connswater/fitness-classes1/{today}/by-time"
+
+def error_exit(browser, status):
+    browser.close()
+    notification.send_notification(status, targetDate)
+    exit(1)
 
 def book_pilates():
     with sync_playwright() as p:
@@ -28,59 +33,55 @@ def book_pilates():
         print(f"Attempting to open site {bookingLink}")
         page.goto(bookingLink, wait_until="networkidle")
         #Cookie block
-        print("Checking for cookies pop-up")
+        print("ACTION: Checking for cookies pop-up")
         try:
             page.locator("#onetrust-reject-all-handler").click(timeout=defaultTimeout)
-            print("Cookies rejected")
+            print("STATUS: Cookies rejected")
         except PlaywrightTimeoutError:
-            print("No cookies to reject")
+            print("STATUS: No cookies to reject")
             
         #log-in block
         try:
+            print("ACTION: Attempting to log in")
             page.get_by_test_id("login").click()
             page.locator("#username").fill(username)
             page.locator("#password").fill(password)
             page.get_by_role("button", name="Log in").click(timeout=defaultTimeout)
-            print("Logged in")
+            print("STATUS: Logged in")
         except:
-            print("Issue entering log in details")
-            browser.close()
-            return
+            error_exit(browser, "Login Error")
         #Clearing Cart
         try:
-            print("Checking if Cart is empty")
+            print("ACTION: Checking if Cart is empty")
             page.get_by_label(re.compile("Remove from cart")).click(timeout=defaultTimeout)
             success_message = page.get_by_text("Successfully removed item from the basket.")
             success_message.wait_for(state="visible")
-            print("Cart succesfully cleared")
+            print("STATUS: Cart succesfully cleared")
         except:
-            print("Cart already empty")
+            print("STATUS: Cart already empty")
             
         #Navigating to Pilates class
         try:
-            print("Navigating to Pilates class...")
+            print("ACTION: Navigating to Pilates class...")
             page.get_by_test_id(f"date-{targetDate}").click(timeout=defaultTimeout)
             page.get_by_label(re.compile("Pilates")).click(timeout=defaultTimeout)
-            print("Pilates class selected")
+            print("STATUS: Pilates class selected")
         except PlaywrightTimeoutError:
-            print(f"Error occurred while navigating to Pilates class.\n{targetDate} is either not available for selection yet or pilates is not available on {targetDate}..")
-            browser.close()
-            return
+            error_exit(browser, "Navigation Error")
         
         #Main booking process
         try:
-            print("Adding to basket...")
+            print("ACTION: Adding to basket...")
             page.get_by_role("button", name="Book now").click(timeout=defaultTimeout)
-            print("Confirming booking...")
+            print("ACTION: Confirming booking...")
             page.get_by_role("button", name="Pay now").click(timeout=defaultTimeout)
-            print("Waiting for booking confirmation URL...")
+            print("STATUS: Waiting for booking confirmation URL...")
             page.wait_for_url("**/booking-confirmed/**", timeout=15000)
-            print(f"Pilates booked for {targetDate}!!!!")
+            print(f"STATUS: Pilates booked for {targetDate} :)")
         except PlaywrightTimeoutError:
-            print("Error booking the pilates class.")
-            browser.close()
-            return
+            error_exit(browser, "Booking Error")
         browser.close()
+        notification.send_notification("Success", targetDate)
 
 if __name__ == "__main__":
     book_pilates()
